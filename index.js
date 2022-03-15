c5073c8bca82dc235d85237ddf044d28d7d7e54b
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2022-03-15 10:15:00 +0000

    Discord Chatbot 🗨

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ app.get('/', (req, res) => res.send('Created By CodeWhiteWeb'))
 app.listen(port, () =>
     console.log(`Your app is listening to http://localhost:${port}`)
 )
-
 const client = new Discord.Client();
+client.login(process.env.token);
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
-  client.user.setActivity(`Created By CodeWhiteWeb`);
+  client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
 });
+
 client.on("message", async message => {
-if (message.channel.name == process.env.channel) {
+if (message.channel.name == "chatbot") {
 if (message.author.bot) return;
 message.content = message.content.replace(/@(everyone)/gi, "everyone").replace(/@(here)/gi, "here");
 if (message.content.includes(`@`)) {
@@ -23,11 +24,10 @@ return message.channel.send(`**:x: Please dont mention anyone**`);
  }
   message.channel.startTyping();
 if (!message.content) return message.channel.send("Please say something.");
-    let msg = message.content
-fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${msg}`)
+fetch(`http://api.brainshop.ai/get?bid=164757&key=ajEOfoBjQ7pjKAVM&uid=1&msg=${encodeURIComponent(message.content)}`)
     .then(res => res.json())
     .then(data => {
-        message.channel.send(`> ${message.content} \n <@${message.author.id}> ${data.message}`);
+        message.channel.send(`> ${message.content} \n <@${message.author.id}> ${data.cnt}`);
     });
       message.channel.stopTyping();
 }
